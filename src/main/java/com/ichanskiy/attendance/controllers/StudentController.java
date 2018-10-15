cecf368f1e8e7cf5631e1f1a5241d6c740067f7e
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-10-15 23:24:57 +0300

    create student controller

---
+++ src/main/java/com/ichanskiy/attendance/controllers/StudentController.java
@@ -0,0 +1,23 @@
+package com.ichanskiy.attendance.controllers;
+
+import com.ichanskiy.attendance.entity.Student;
+import com.ichanskiy.attendance.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("/all")
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
+    }
+}
